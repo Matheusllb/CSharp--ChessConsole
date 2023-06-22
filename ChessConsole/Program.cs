d13a337fc24139f8edbbebec046df8ef30212eba
Matheus <matheusluislopesbatista@gmail.com>
2023-06-21 22:13:05 -0300

    Criando a classe ChessPosition (PosicaoXadrez)

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -12,12 +12,12 @@ namespace ChessConsole
             {
                 Board b = new Board(8, 8);
 
-                b.AddPiece(new Rook(b, Color.Black), new Position(0, 0));
-                b.AddPiece(new Knight(b, Color.Black), new Position(1, 3));
-                b.AddPiece(new King(b, Color.Black), new Position(2, 4));
-                b.AddPiece(new Bishop(b, Color.Black), new Position(9, 4)); //Testing Exception
+                ChessPosition pos = new ChessPosition('a', 1);
 
                 Screen.PrintScreen(b);
+
+                Console.WriteLine(pos);
+                Console.WriteLine(pos.ToPosition());
 
                 Console.ReadLine();
             }
