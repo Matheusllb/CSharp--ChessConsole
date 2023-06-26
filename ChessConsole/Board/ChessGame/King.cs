b614be15117f7c8c1f1b54ac1514be56f283b39e
Matheus <matheusluislopesbatista@gmail.com>
2023-06-26 20:41:40 -0300

    Implementando movimentos possiveis do Rei

--- ChessConsole/Board/ChessGame/King.cs
+++ ChessConsole/Board/ChessGame/King.cs
@@ -13,5 +13,70 @@ namespace ChessGame
         {
             return "R"; //King = Rei
         }
+
+        private bool CanMove(Position pos)
+        {
+            Piece p = Board.piece(pos);
+            return p == null || p.Color != this.Color;
+        }
+
+        override public abstract bool[,] PossibleMoves()
+        {
+            bool[,] matrix = new bool[Board.Lines, Board.Columns];
+
+            Position pos = new Position(0, 0);
+
+            //Up
+            pos.DefineValues(Position.Line - 1, Position.Column);
+            if(Board.ValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Line, pos.Column] = true;
+            }
+            //North East
+            pos.DefineValues(Position.Line - 1, Position.Column + 1);
+            if(Board.ValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Line, pos.Column] = true;
+            }
+            //Right
+            pos.DefineValues(Position.Line, Position.Column + 1);
+            if(Board.ValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Line, pos.Column] = true;
+            }
+            //Southeast
+            pos.DefineValues(Position.Line + 1, Position.Column + 1);
+            if(Board.ValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Line, pos.Column] = true;
+            }
+            //Down
+            pos.DefineValues(Position.Line + 1, Position.Column);
+            if(Board.ValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Line, pos.Column] = true;
+            }
+            //South-West
+            pos.DefineValues(Position.Line + 1, Position.Column - 1);
+            if(Board.ValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Line, pos.Column] = true;
+            }
+            //Left
+            pos.DefineValues(Position.Line, Position.Column - 1);
+            if(Board.ValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Line, pos.Column] = true;
+            }
+            //Northwest
+            pos.DefineValues(Position.Line - 1, Position.Column - 1);
+            if(Board.ValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Line, pos.Column] = true;
+            }
+
+            return matrix;
+        }
+
     }
 }
